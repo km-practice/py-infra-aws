08d31598c178c60ba26cffdcf6b06da5df1f50ab
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-18 11:26:20 +0100

    wcfer

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "your-terraform-state-bucket"
-    key    = "path/to/your/terraform.tfstate"
     region = "us-west-2"
+    key    = ""  # This will be dynamically configured in the workflow
   }
 }
+

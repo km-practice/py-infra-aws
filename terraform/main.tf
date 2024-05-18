68771262b649898219ba9d2370f8e79d04562f9d
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-18 11:19:12 +0100

    dbfb

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "example" {
-  source = "./modules/aws_module"
+  source = "./modules/ec2"
   ami           = var.ami
   instance_type = var.instance_type
   instance_name = var.instance_name

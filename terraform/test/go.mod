7f99b3e80d22f3246d2ab36f6f8bd9c6db55e8c0
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-18 14:30:48 +0100

    hgcgfx

---
+++ terraform/test/go.mod
@@ -0,0 +1,7 @@
+module example.com/terraform-test
+
+go 1.16
+
+require (
+    github.com/gruntwork-io/terratest/modules/terraform v0.38.8
+)
---
+++ terraform/test/terratest_test.go
@@ -0,0 +1,21 @@
+package test
+
+import (
+    "testing"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorld(t *testing.T) {
+    t.Parallel()
+
+    terraformOptions := &terraform.Options{
+        TerraformDir: "../",
+    }
+
+    defer terraform.Destroy(t, terraformOptions)
+    terraform.InitAndApply(t, terraformOptions)
+
+    output := terraform.Output(t, terraformOptions, "example_output")
+    assert.Equal(t, "Hello, World!", output)
+}

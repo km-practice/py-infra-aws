5f7cae2da616d14518f130a07854404a921000cb
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-18 14:31:49 +0100

    ggfxgf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "example" {
-  source = "./modules/ec2"
+  source        = "./modules/ec2"
   ami           = var.ami
   instance_type = var.instance_type
   instance_name = var.instance_name

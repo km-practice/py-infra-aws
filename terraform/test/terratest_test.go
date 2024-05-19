ce8cf08228e10b1ef1263b8ebe6e34dd7d934f49
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-19 19:07:08 +0100

    dhdhf

--- terraform/test/terratest_test.go
+++ terraform/test/terratest_test.go
@@ -1,34 +1,29 @@
 package test
 
 import (
+    "fmt"
+    "os"
     "path/filepath"
     "testing"
+
     "github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-// TestTerraformBasicExample uses Terratest to test a basic Terraform configuration.
 func TestTerraformBasicExample(t *testing.T) {
-    t.Parallel()
+    cwd, err := os.Getwd()
+    if err != nil {
+        t.Fatal(err)
+    }
+    fmt.Println("Current working directory:", cwd)
 
-    // The path to where your Terraform configuration files are located
     terraformOptions := &terraform.Options{
-        // Set the path to the Terraform code that will be tested.
         TerraformDir: "../",
-
-        // Variables file to pass to our Terraform code
-        VarFiles: []string{filepath.Join("..", "environments", "dev.tfvars")},
-
-        // Disable colors in Terraform commands so output isn't muddled
-        NoColor: true,
+        VarFiles: []string{filepath.Join(cwd, "..", "environments", "dev.tfvars")},
     }
 
-    // Clean up resources with "terraform destroy" at the end of the test.
     defer terraform.Destroy(t, terraformOptions)
-
-    // Initialize and apply the Terraform code in the directory
-    if _, err := terraform.InitAndApplyE(t, terraformOptions); err != nil {
-        t.Fatalf("Failed to apply Terraform: %v", err)
-    }
+    terraform.InitAndApply(t, terraformOptions)
 }
+
 
 

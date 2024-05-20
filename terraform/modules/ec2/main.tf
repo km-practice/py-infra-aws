b299a219fb9dafb196a13981784bfd80bff702ad
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-20 09:59:24 +0100

    dfbg

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
 
+  metadata_options {
+
+       http_endpoint = "enabled"
+       http_tokens   = "optional"
+ }
+
   tags = {
     Name = var.instance_name
   }

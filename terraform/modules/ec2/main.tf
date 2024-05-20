1a9ae3bc4a735a716e7b368ddc9db7e15fb538a3
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-20 10:08:25 +0100

    hgxfg

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,12 +1,14 @@
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
+  monitoring    = true
+  ebs_optimized = true
 
   metadata_options {
 
-       http_endpoint = "enabled"
-       http_tokens   = "required"
- }
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
 
   tags = {
     Name = var.instance_name

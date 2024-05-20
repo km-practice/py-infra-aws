a8eebc8031f0e13a25f49867c932c5ed8dd64a16
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-20 15:07:15 +0100

    hgc

--- terraform/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bbucket" {
-  bucket = "kjftypractice-${terraform.workspace}"
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.bbucket.id
-  acl    = "private"
-}
-

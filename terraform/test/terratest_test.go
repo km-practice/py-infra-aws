bd6aa1095ab9e2afd7c781a0b3d1cde734b480e2
Kudzai Mtoko <kudzaimtoko@gmail.com>
2024-05-20 14:28:38 +0100

    assdgf

--- terraform/test/terratest_test.go
+++ terraform/test/terratest_test.go
@@ -6,8 +6,23 @@ import (
     "path/filepath"
     "testing"
 
+    "github.com/gruntwork-io/terratest/modules/aws"
     "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
 )
+
+// Example unit test function
+func TestAddition(t *testing.T) {
+    result := add(2, 3)
+    expected := 5
+    if result != expected {
+        t.Errorf("Expected %d but got %d", expected, result)
+    }
+}
+
+func add(a int, b int) int {
+    return a + b
+}
 
 func TestTerraformBasicExample(t *testing.T) {
     cwd, err := os.Getwd()
@@ -24,6 +39,33 @@ func TestTerraformBasicExample(t *testing.T) {
     defer terraform.Destroy(t, terraformOptions)
     terraform.InitAndApply(t, terraformOptions)
 }
+
+func TestEndToEnd(t *testing.T) {
+    cwd, err := os.Getwd()
+    if err != nil {
+        t.Fatal(err)
+    }
+    fmt.Println("Current working directory:", cwd)
+
+    terraformOptions := &terraform.Options{
+        TerraformDir: "../",
+        VarFiles: []string{filepath.Join(cwd, "..", "environments", "dev.tfvars")},
+    }
+
+    defer terraform.Destroy(t, terraformOptions)
+    terraform.InitAndApply(t, terraformOptions)
+
+    // Example: Validate AWS resources
+    instanceID := terraform.Output(t, terraformOptions, "instance_id")
+    region := "eu-west-2"
+    aws.AssertEC2InstanceRunning(t, region, instanceID)
+
+    // Example: Add more end-to-end checks
+    instanceState := aws.GetEC2InstanceState(t, region, instanceID)
+    assert.Equal(t, "running", instanceState)
+}
+
+
 
 
 
